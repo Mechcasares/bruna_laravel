ecef31d1dda539095d1c4810eee8b70013f5bbd3
Mercedes Casares <mercedescasares@MacBook-Pro-de-Mech.local>
2019-07-06 15:34:47 -0300

    log out funcionando

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BRUNA</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -15,41 +15,43 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+   
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<!-- aca arranca la de bruna -->
+<section class="container">
+		
+    <section class="navbarright">
+        <article class="col-12 col-md-12 col-lg-12">
+            <nav>
+                <ul>
+                    <li class="links">
+                        <a href="/">HOME</a>
+                        <a href="/cart">PRODUCTOS</a>
+                        <a href="#">NOSOTROS</a>
+                        <a href="#">CONTACTO</a> 
+                    </li>
+                </ul>
+            </nav>
+        </article>
+    </section>
+        
+    <section class="logo">
+        <a href="/"><img src="img/bruna.png"></a>
+    </section>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
+    <section class="icon-bar">
+        <article class="col-12 col-md-12 col-lg-12">
+            <a href="#"><i class="fa fa-heart"></i></a>
+            <a href="#"><i class="fa fa-search"></i></a> 
+            <a href="#"><i class="fa fa-shopping-cart"></i></a>
+            @guest
+            <a href="/login"><i class="fa fa-user"></i></a>
+            @else 
+            <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
@@ -67,14 +69,16 @@
                                 </div>
                             </li>
                         @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+            
+             <!-- Right Side Of Navbar -->
+            
+           
+        </article>
+    </section>
+
+</section>
+
+ 
 </body>
 </html>
