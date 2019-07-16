d93c58fd3b6bdea41302d55e948228a310ea6ea2
Mercedes Casares <mercedescasares@MacBook-Pro-de-Mech.local>
2019-07-16 19:32:23 -0300

    boton

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -12,10 +12,13 @@
       @if($product->photopath !== null)
       <img src="/storage/{{ $product->photopath }}" alt="photo">
       @endif
+      <br> <button type="button" href="/cart/add/{{ $movie->id }}">Agregar</button> 
     </div>
      </div>
       
      
+
+
   @endforeach
 </div>
 </main>
